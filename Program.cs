4592e75c02825853f0caa9734542306febc75e50
Jesper Wallén <jesperwallen2001@gmail.com>
2024-01-22 14:21:48 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/add", (int num1, int num2) => AddNumbers(num1, num2));
+app.MapGet("/sub", (int num1, int num2) => SubNumbers(num1, num2));
+
+app.Run();
+
+string AddNumbers(int num1, int num2)
+{
+    return $"Summan av {num1} + {num2} = {num1 + num2}";
+}
+string SubNumbers(int num1, int num2)
+{
+    return $"Summan av {num1} - {num2} = {num1 - num2}";
+}
